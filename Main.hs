2c52834271741e23bb77efec31f8f97e02fe90b1
Noam Lewis <jones.noamle@gmail.com>
2015-09-24 01:55:54 +0300

    fix some warnings

--- Main.hs
+++ Main.hs
@@ -14,20 +14,10 @@ import Data.Map (Map)
 import qualified Data.Set as Set
 import Data.Set (Set)
 
--- import Control.Monad.Identity
--- import Control.Monad.Trans
--- import Control.Monad.Trans.Error
--- import Data.Foldable
--- import Data.Traversable
-
--- import Control.Monad (foldM)
--- import Control.Monad (when)
--- import Control.Monad.ST
 import Control.Monad.Trans.Class (lift)
-import Control.Monad.Trans.Either (EitherT, runEitherT, left, bimapEitherT)
+import Control.Monad.Trans.Either (EitherT, runEitherT)
 import Control.Monad.Trans.Reader (ReaderT, ask, local, runReaderT)
 import Control.Monad.Trans.State (evalStateT, get, put)
--- import Data.STRef
 
 type VarName = String
 
@@ -163,92 +153,6 @@ typeOf (FExpr e') = case e' of
         withVar n tDef $ typeOf e2
 -- ----------------------------------------------------------------------
 
--- gen :: Type s -> Infer s (Type s)
--- gen t@(Fix (TVar ioTV)) = do
---     tv <- readCell ioTV
---     case tv of
---         Unbound name level -> do
---             curLevel <- currentLevel
---             return $ if curLevel < level
---                      then Fix $ QVar name
---                      else t
---         Link t' -> gen t'
--- gen (Fix (TArrow ta tb l)) = do
---     gta <- gen ta
---     gtb <- gen tb
---     return $ Fix $ TArrow gta gtb l
--- gen t = return t
-
--- ----------------------------------------------------------------------
-
--- newTVar :: Infer s (Type s)
--- newTVar = do
---     tvName <- fresh
---     level <- currentLevel
---     Fix . TVar <$> newCell (Unbound tvName level)
-
--- newArrow :: Type s -> Type s -> Infer s (Type s)
--- newArrow t1 t2 = do
---     lOld <- newCell 0
---     lNew <- newCell 0
---     return . Fix . TArrow t1 t2 $ Levels lOld lNew
-
--- type TEnv s = [(QName, Type s)]
-
--- tenvEmpty :: TEnv s
--- tenvEmpty = []
-
--- inst' :: TEnv s -> Type s -> Infer s (Type s, TEnv s)
--- inst' env (Fix (QVar name)) =
---     case lookup name env of
---     Just t -> return (t, env)
---     Nothing -> do
---         tv <- newTVar
---         return (tv, (name, tv) : env)
--- inst' env t@(Fix (TVar ioTV)) = do
---     tv <- readCell ioTV
---     case tv of
---         Unbound _ _ -> return (t, env)
---         Link tLink -> inst' env tLink
--- inst' env (Fix (TArrow ta tb l)) = do
---     (ta', envA) <- inst' env ta
---     (tb', envB) <- inst' envA tb
---     return (Fix $ TArrow ta' tb' l, envB)
-
--- inst :: Type s -> Infer s (Type s)
--- inst t = fst <$> inst' tenvEmpty t
-
--- type VEnv s = [(VarName, Type s)]
-
--- typeOf :: VEnv s -> Expr -> Infer s (Type s)
--- typeOf env (Var name) =
---     case lookup name env of
---     Just t -> inst t
---     Nothing -> typeError $ UnboundVar name
--- typeOf env (App e1 e2) = do
---     tFun <- typeOf env e1
---     tArg <- typeOf env e2
---     tRes <- newTVar
---     old <- newCell 0
---     new <- newCell 0
---     unify tFun $ Fix $ TArrow tArg tRes (Levels old new) -- hack
---     return tRes
--- typeOf env (Lam name e) = do
---     tArg <- newTVar
---     tBody <- typeOf ((name,tArg) : env) e
---     old <- newCell 0
---     new <- newCell 0
---     return $ Fix $ TArrow tArg tBody (Levels old new) -- hack
--- typeOf env (Let name e1 e2) = do
---     enterLevel
---     tDef <- typeOf env e1 -- non-recursive let
---     leaveLevel
---     tVar <- gen tDef
---     typeOf ((name,tVar) : env) e2
-
-
--- --infer :: Expr -> Either TypeError PureType
--- --infer = runInfer . typeOf []
 
 test_id_inner = FExpr (Lam "x" $ FExpr $ Var "x")
 test_id = FExpr $ Let "id" test_id_inner (FExpr $ Var "id") -- (FExpr $ App (FExpr $ Var "id") (FExpr $ Var "id")) -- (FExpr $ Var "id")
