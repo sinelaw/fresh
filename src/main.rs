7465bf3f9b629e1f13a2684b015445b5df530021
Noam Lewis <lenoam@gmail.com>
2024-12-24 22:06:55 +0200

    WIP

--- src/main.rs
+++ src/main.rs
@@ -5,25 +5,46 @@ use std::io;
 use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyModifiers};
 use ratatui::{DefaultTerminal, Frame};
 
-fn main() -> io::Result<()> {
-    let terminal = ratatui::init();
-    let result = run(terminal);
-    ratatui::restore();
-    result
+struct State {
+    text: Vec<char>,
 }
 
-fn run(mut terminal: DefaultTerminal) -> io::Result<()> {
-    loop {
-        terminal.draw(render)?;
-        let event = event::read()?;
+impl State {
+    fn run(&mut self, mut terminal: DefaultTerminal) -> io::Result<()> {
+        loop {
+            terminal.draw(|x| self.render(x))?;
+            let event = event::read()?;
 
-        match event {
-            Event::Key(  KeyEvent{ code : KeyCode::Char('q'), modifiers: KeyModifiers::CONTROL, ..  } ) => break Ok(()),
-            _ => {}
+            match event {
+                Event::Key(KeyEvent {
+                    code: KeyCode::Char('q'),
+                    modifiers: KeyModifiers::CONTROL,
+                    ..
+                }) => break Ok(()),
+                Event::Key(KeyEvent {
+                    code: KeyCode::Char(c),
+                    modifiers: KeyModifiers::NONE,
+                    ..
+                }) => self.insert_char(c),
+                _ => {}
+            }
         }
+    }
+
+    fn insert_char(&mut self, c: char) {
+        self.text.push(c);
+    }
+
+    fn render(&self, frame: &mut Frame) {
+        let s: String = self.text.iter().collect();
+        frame.render_widget(s, frame.area());
     }
 }
 
-fn render(frame: &mut Frame) {
-    frame.render_widget("hello world", frame.area());
+fn main() -> io::Result<()> {
+    let terminal = ratatui::init();
+    let mut state: State = State { text: Vec::new() };
+    let result = state.run(terminal);
+    ratatui::restore();
+    result
 }
