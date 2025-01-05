ae71ef987ace02b29a03ace6414280ac493a50d5
Noam Lewis <noam@example.com>
2025-01-05 22:13:40 +0200

    WIP

--- src/memstore.rs
+++ src/memstore.rs
@@ -3,34 +3,38 @@ pub enum Chunk {
     Loaded { data: Vec<u8>, need_store: bool },
     Empty,
 }
-
-pub struct Memstore<'a> {
-    chunks: HashMap<u64, Chunk>,
-    chunk_size: u64,
-    load_fn: Box<dyn Fn(u64) -> Option<Vec<u8>> + 'a>,
-    store_fn: Box<dyn Fn(u64, &[u8]) + 'a>,
+trait LoadStore {
+    fn load(&self, offset: u64) -> Option<Vec<u8>>;
+    fn store(&self, offset: u64, data: &[u8]);
 }
 
-impl<'a> Memstore<'a> {
-    pub fn new(
-        chunk_size: u64,
-        load_fn: impl Fn(u64) -> Option<Vec<u8>> + 'a,
-        store_fn: impl Fn(u64, &[u8]) + 'a,
-    ) -> Memstore<'a> {
+pub struct Memstore<L>
+where
+    L: LoadStore,
+{
+    chunks: HashMap<u64, Chunk>,
+    chunk_size: u64,
+    load_store: L,
+}
+
+impl<L> Memstore<L>
+where
+    L: LoadStore,
+{
+    pub fn new(chunk_size: u64, load_store: L) -> Memstore<L> {
         Memstore {
             chunks: HashMap::new(),
             chunk_size,
-            load_fn: Box::new(load_fn),
-            store_fn: Box::new(store_fn),
+            load_store,
         }
     }
 
-    pub fn get(&mut self, offset: u64) -> &Chunk {
+    pub fn get(&mut self, offset: u64) -> &mut Chunk {
         let chunk_index = offset / self.chunk_size;
 
-        let load = &self.load_fn;
+        let load_store = &self.load_store;
         return self.chunks.entry(chunk_index).or_insert_with_key(|v| {
-            if let Some(data) = (load)(*v) {
+            if let Some(data) = load_store.load(*v) {
                 Chunk::Loaded {
                     data,
                     need_store: false,
@@ -42,7 +46,7 @@ impl<'a> Memstore<'a> {
     }
 
     pub fn store_all(&mut self) {
-        let store = &self.store_fn;
+        let load_store = &self.load_store;
         for (index, chunk) in self.chunks.iter_mut() {
             if let Chunk::Loaded {
                 data,
@@ -50,10 +54,38 @@ impl<'a> Memstore<'a> {
             } = chunk
             {
                 if *is_modified {
-                    (store)(*index, data);
+                    load_store.store(*index, data);
                     *is_modified = false;
                 }
             }
         }
+    }
+
+    pub fn iter_at(&mut self, offset: u64) -> ChunkIter<L> {
+        ChunkIter {
+            index: offset / self.chunk_size,
+            memstore: self,
+        }
+    }
+}
+
+pub struct ChunkIter<'a, L>
+where
+    L: LoadStore,
+{
+    memstore: &'a mut Memstore<L>,
+    index: u64,
+}
+
+impl<'a, L> Iterator for ChunkIter<'a, L>
+where
+    L: LoadStore,
+{
+    type Item = &'a Chunk;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let chunk = self.memstore.get(self.index * self.memstore.chunk_size);
+        self.index += 1;
+        Some(chunk)
     }
 }
